46e0abcc212565e584a2f3ecbc3e606ca176f634
Lucas Silveira <lugano1995@gmail.com>
2020-10-16 17:16:01 -0300

    feat: create main function and env variables

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	AnimalRepository "webapp/datastore/animal"
+	HttpAnimal "webapp/delivery/animal"
+	"webapp/driver"
+)
 
+func main() {
+	config := driver.MySQLConfig{
+		Host:     os.Getenv("SQL_HOST"),
+		User:     os.Getenv("SQL_USER"),
+		Password: os.Getenv("SQL_PASSWORD"),
+		Port:     os.Getenv("SQL_PORT"),
+		Db:       os.Getenv("SQL_DB"),
+	}
+
+	var err error
+
+	db, err := driver.ConnectToMySQL(config)
+
+	if err != nil {
+		log.Println("Could not connect to sql, err:", err)
+		return
+	}
+
+	animalRepository := AnimalRepository.New(db)
+	httpAnimal := HttpAnimal.New(animalRepository)
+
+	http.HandleFunc("/animal", httpAnimal.Handler)
+	fmt.Println(http.ListenAndServe(":3000", nil))
 }
